809421395ed215b2a5d51898ff98cbf11996873c
Thiago Contelli <thiagocontellid@gmail.com>
2022-11-13 15:25:43 -0300

    feat: registration page

--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,8 +1,30 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+
 import * as M from '@mui/material';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 import './style.css';
 
+import { useRegistration } from './useRegistration';
+
 export function Registration() {
+	const {
+		allBooks,
+		author,
+		description,
+		getAllBooks,
+		name,
+		registerBook,
+		setAuthor,
+		setDescription,
+		setName,
+		bookBase64,
+		setBookBase64,
+		handleUploadFile,
+		file,
+		setFile,
+	} = useRegistration();
+
 	return (
 		<M.Box
 			sx={{
@@ -10,24 +32,91 @@ export function Registration() {
 				flexDirection: 'column',
 				alignItems: 'center',
 				justifyContent: 'center',
-				gap: '1rem',
+				gap: '2rem',
 				width: '100%',
 				height: '100vh',
 			}}
 		>
-			<h2>User Registration</h2>
+			<h2>Register your book</h2>
 			<form>
-				<M.TextField label='Name' />
 				<M.TextField
-					label='E-mail'
-					type='email'
+					label='Name'
+					required
+					value={name}
+					onChange={(
+						e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+					) => {
+						setName(e.target.value);
+					}}
 				/>
 				<M.TextField
-					label='Password'
-					type='password'
+					label='Author'
+					required
+					value={author}
+					onChange={(
+						e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+					) => {
+						setAuthor(e.target.value);
+					}}
 				/>
-				<M.Button variant='contained'>Submit</M.Button>
+				<M.TextField
+					label='Description'
+					required
+					value={description}
+					multiline
+					minRows={3}
+					maxRows={8}
+					onChange={(
+						e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+					) => {
+						setDescription(e.target.value);
+					}}
+				/>
+				<M.Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+					<M.Button
+						variant='outlined'
+						startIcon={<FileUploadIcon />}
+						component='label'
+					>
+						<input
+							hidden
+							accept='application/pdf'
+							type='file'
+							multiple
+							onChange={e => {
+								handleUploadFile(e);
+							}}
+						/>
+						{file ? file!.name : 'Upload book'}
+					</M.Button>
+					<M.Button
+						type='submit'
+						variant='contained'
+						onClick={e => {
+							e.preventDefault();
+							registerBook();
+						}}
+					>
+						Register
+					</M.Button>
+					{/* <M.Button
+						type='submit'
+						variant='outlined'
+						onClick={e => {
+							e.preventDefault();
+							getAllBooks();
+						}}
+					>
+						List
+					</M.Button> */}
+				</M.Box>
 			</form>
+
+			<ul>
+				{allBooks.map((book, index) => (
+					<li key={index}>{book.name}</li>
+				))}
+			</ul>
 		</M.Box>
 	);
 }
---
+++ src/pages/Registration/useRegistration.tsx
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export interface Book {
+	name: string;
+	author: string;
+	description: string;
+	bookBase64: string;
+}
+
+export function useRegistration() {
+	const [allBooks, setAllBooks] = useState<Book[]>([]);
+
+	// form fields
+	const [name, setName] = useState('');
+	const [author, setAuthor] = useState('');
+	const [description, setDescription] = useState('');
+	const [bookBase64, setBookBase64] = useState<string | ArrayBuffer | null>('');
+	const [file, setFile] = useState<File>();
+
+	async function registerBook() {
+		try {
+			const response = axios.post('http://localhost:3333/books', {
+				name,
+				author,
+				description,
+				bookBase64,
+			});
+			resetInputFields();
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async function getAllBooks() {
+		const response = await axios.get('http://localhost:3333/books');
+		setAllBooks(response.data);
+	}
+
+	function handleUploadFile(e: React.ChangeEvent<HTMLInputElement>) {
+		const selectedFile = e.target.files![0];
+		setFile(selectedFile);
+
+		if (selectedFile) {
+			const fileReader = new FileReader();
+			fileReader.readAsDataURL(selectedFile);
+			fileReader.onloadend = () => {
+				setBookBase64(fileReader.result);
+			};
+		}
+	}
+
+	function resetInputFields() {
+		setName('');
+		setAuthor('');
+		setDescription('');
+		setFile(undefined);
+	}
+
+	return {
+		registerBook,
+		getAllBooks,
+		allBooks,
+		description,
+		setDescription,
+		author,
+		setAuthor,
+		name,
+		setName,
+		bookBase64,
+		setBookBase64,
+		handleUploadFile,
+		file,
+		setFile,
+	};
+}
